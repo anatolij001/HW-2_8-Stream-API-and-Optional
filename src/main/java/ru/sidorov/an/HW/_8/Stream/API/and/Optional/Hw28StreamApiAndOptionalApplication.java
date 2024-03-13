93fc4effba5bd90fb0059b2160b153217d1ebaf1
anatolij <anatolij001@gmail.com>
2024-03-13 15:44:00 +0300

    Initial Commit.

---
+++ src/main/java/ru/sidorov/an/HW/_8/Stream/API/and/Optional/Hw28StreamApiAndOptionalApplication.java
@@ -0,0 +1,13 @@
+package ru.sidorov.an.HW._8.Stream.API.and.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw28StreamApiAndOptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw28StreamApiAndOptionalApplication.class, args);
+	}
+
+}
